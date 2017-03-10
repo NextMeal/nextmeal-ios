76c3421a8da22144429c41cb58084b166ea4df71
Anson Liu <ansonl@users.noreply.github.com>
2017-03-09 20:20:24 -0500

    Initial Commit

---
+++ nextmeal/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  nextmeal
+//
+//  Created by Anson Liu on 3/9/17.
+//  Copyright © 2017 Anson Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ nextmeal/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  nextmeal
+//
+//  Created by Anson Liu on 3/9/17.
+//  Copyright © 2017 Anson Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
